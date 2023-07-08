f6c88a984cbc404d733213a955b4acfd8e0fa268
danielroman11 <danielroman.05111@gmail.com>
2023-07-07 19:15:43 -0500

    FIX: Ahead changes on brach, Cambios recientes

--- src/js/agregarImagen.js
+++ src/js/agregarImagen.js
@@ -8,13 +8,8 @@ Dropzone.options.imagen = {
   dictDefaultMessage: "Sube tu imagen aquí",
   acceptedFiles: ".png,.jpg,.jpeg",
   maxFilesize: 4,
-<<<<<<< HEAD
-  maxFiles: 3,
-  parallelUploads: 3,
-=======
   maxFiles: 1,
   parallelUploads: 1,
->>>>>>> 1a87c01cdaa6de02955d3462fe60f1ad495308e9
   autoProcessQueue: false,
   addRemoveLinks: true,
   dictRemoveFile: "Borrar Imágen",
@@ -23,16 +18,6 @@ Dropzone.options.imagen = {
     'CSRF-Token': token
   },
   paramName: 'imagen',
-<<<<<<< HEAD
-  init: function() {
-    const dropzone = this;
-    const btnPublicar = document.getElementById("btnPublicar")
-    btnPublicar.addEventListener("click", e => {
-      dropzone.getQueuedFiles();
-    })
-  }
-}
-=======
   init: function () {
     const dropzone = this;
     const btnUpload = document.getElementById("publicar");
@@ -48,4 +33,3 @@ Dropzone.options.imagen = {
     });
   }
 }
->>>>>>> 1a87c01cdaa6de02955d3462fe60f1ad495308e9
