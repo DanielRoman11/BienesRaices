1683faea0131a52b628e3e1ac533752b976daba7
danielroman11 <danielroman.05111@gmail.com>
2023-07-22 12:44:07 -0500

    Fix: Zoom en el mapa

--- src/js/mostrarMapa.js
+++ src/js/mostrarMapa.js
@@ -1,10 +1,14 @@
 (function() {
   const lat = document.getElementById("lat").textContent;
   const lng = document.getElementById("lng").textContent;
+
   const calle = document.getElementById("calle").textContent;
 
-  var mapa = L.map('mapa').setView([lat, lng ], 13);
-
+  var mapa = L.map('mapa', {
+    fullscreenControl: {
+      pseudoFullscreen: false
+    }
+  }).setView([lat, lng ], 15);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -12,11 +16,11 @@
 
   //* Crear Marcador
   let marker = new L.marker([lat, lng], {
-    color: 'green',
-    fillColor: '#2ADB3B',
+    color: 'red',
+    fillColor: '#fffff',
+    fillOpacity: 0.4,
     draggable: false,
   }).addTo(mapa);
 
   marker.bindPopup(`Dirección: ${calle}`);
-
 })();
