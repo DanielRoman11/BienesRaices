0dee9e2c775cb94cea2b7bf96ee26d31119a6ed1
danielroman11 <danielroman.05111@gmail.com>
2023-07-21 20:44:13 -0500

    addon: configuracion webpack y vista del mapa

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ export default {
   mode: "development",
   entry: {
     mapa: "./src/js/mapa.js",
-    agregarImagen: "./src/js/agregarImagen.js"
+    agregarImagen: "./src/js/agregarImagen.js",
+    mostrarMapa: "./src/js/mostrarMapa.js",
   },
   output:{
     path: path.resolve("public/js"),
