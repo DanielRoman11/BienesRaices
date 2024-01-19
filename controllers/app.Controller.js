81e14ced5ac8e8d75b32293ad8a3a87dd0a3ab98
Daniel Román <danielroman.05111@gmail.com>
2024-01-19 00:30:03 -0500

    refactor(webpack): using another command to run weboack and watch

--- controllers/app.Controller.js
+++ controllers/app.Controller.js
@@ -1,10 +1,24 @@
 import { Categoria, Precio, Propiedad } from "../models/index.js"
 
 export const home = async(req,res) => {
-  const [ categorias, precios, casas ] = await Promise.all([
+  const [ categorias, precios, casas, apartamentos ] = await Promise.all([
     Categoria.findAll({raw: true}),
     Precio.findAll({raw: true}),
     Propiedad.findAll({
+      limit: 3,
+      include:[{
+        model: Precio,
+        as: 'precio'
+      }],
+      where: {
+        categoriaID: 2
+      },
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    }),
+    Propiedad.findAll({
+      limit: 3,
       include:[{
         model: Precio,
         as: 'precio'
@@ -15,14 +29,17 @@ export const home = async(req,res) => {
       order: [
         ['createdAt', 'DESC']
       ]
-    })
+    }),
+
   ])
 
   res.render('home', {
     pagina: "Home",
     categorias,
     precios,
-    casas
+    casas,
+    apartamentos,
+    
   })
  
  
--- src/js/Mapahome.js
+++ src/js/Mapahome.js
@@ -95,8 +95,5 @@
     }
   }
 
-
-
-  obtenerPropiedades()
-  })();
-  
+    obtenerPropiedades();
+})();
