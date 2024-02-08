9c6226596210aca7d9357eb8518fc9c1b82ed811
Daniel Román <danielroman.05111@gmail.com>
2024-02-08 00:34:00 -0500

    feat(Mensaje) Creando la relación y lógica de negocio para el modelo de mensajes

--- models/index.js
+++ models/index.js
@@ -2,15 +2,20 @@ import Propiedad from "./Propiedad.js"
 import Categoria from "./Categoria.js";
 import Precio from "./Precio.js";
 import Usuario from "./Usuario.js";
+import Mensaje from "./Mensaje.js";
 
 
 Propiedad.belongsTo(Categoria, { foreignKey: "categoriaID" });
 Propiedad.belongsTo(Precio, { foreignKey: "precioID" });
 Propiedad.belongsTo(Usuario, { foreignKey: "usuarioID" });
 
+Mensaje.belongsTo(Propiedad, { foreignKey: 'propiedadID' });
+Mensaje.belongsTo(Usuario, { foreignKey: 'usuarioID' });
+
 export {
   Propiedad, 
   Precio, 
   Categoria, 
-  Usuario
+  Usuario,
+  Mensaje
 }
