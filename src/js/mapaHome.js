a7c56e231b92cfb2731f085e152274436e338bcf
Daniel Román <danielroman.05111@gmail.com>
2024-02-21 22:29:04 -0500

    fix(src>MapaHome.js): Haciendo mandatory el estilo del call to action del popUp

--- src/js/mapaHome.js
+++ src/js/mapaHome.js
@@ -45,7 +45,7 @@
       <strong>Precio:</strong> ${propiedad.precio.nombre} <br>
       <strong>Tratante:</strong> ${propiedad.usuario.nombre} <br>
       <strong>Última Actualización:</strong> ${new Date(propiedad.updatedAt).toLocaleDateString()} <br>
-      <a href="/propiedades/propiedad/${propiedad.id}" class="border-2 border-[#142906] text-[#142906] block p-2 text-center font-bold uppercase">Ver Propiedad</a>
+      <a href="/propiedades/propiedad/${propiedad.id}" class="border-2 border-bodytext !text-bodytext block p-2 text-center font-bold uppercase">Ver Propiedad</a>
       </div>`
       )
 
