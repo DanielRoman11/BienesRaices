dfaabcb9917fb7f07b90d8c92f55e049e745b4da
Daniel Román <danielroman.05111@gmail.com>
2024-02-23 15:24:46 -0500

    fix(Emails) Cambiando las credenciales y manejando errores

--- helpers/emails.js
+++ helpers/emails.js
@@ -3,51 +3,66 @@ import nodemailer from 'nodemailer'
 const transport = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  tls: {
-    ciphers: "SSLv3",
-    rejectUnauthorized: false,
-},
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  tls: {
+    ciphers: "SSLv3",
+    rejectUnauthorized: false,
+  },
 });
 
 const emailRegistro = async (datos) => {
   const { email, nombre, token } = datos
 
-  // Enviar el email
-  await transport.sendMail({
-    from: 'Bienes',
-    to: email,
-    subject: 'Confirma tu cuenta',
-    html: `
-      <p>Hola ${nombre}, comprueba tu cuenta</p>
+  try {
+    const info = await transport.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Confirma tu cuenta',
+      text: "Confirma tu cuenta",
+      html: `
+        <p>Hola ${nombre}, comprueba tu cuenta</p>
+  
+        <p>Tu cuenta ya esta lista, solo debes confirmarla en el siguiente enlace: 
+        <a href="${process.env.BACKEND_URL}/auth/confirmar/${token}">Confirmar cuenta</a> </p>
+  
+        <p>Si no creaste esta cuenta ignora este mensaje</p>
+      `})
 
-      <p>Tu cuenta ya esta lista, solo debes confirmarla en el siguiente enlace: 
-      <a href="${process.env.BACKEND_URL}/auth/confirmar/${token}">Confirmar cuenta</a> </p>
-
-      <p>Si no creaste esta cuenta ignora este mensaje</p>
-    `
-  })
+    console.log("Se envío el email correctamente",info);
+    
+  } catch (error) {
+    console.error("No se pudo enviar el email: ", error);
+  }
 }
+
 const emailOlvidePassword = async (datos) => {
   const { email, nombre, token } = datos
 
-  // Enviar el email
-  await transport.sendMail({
-    from: 'Bienes',
-    to: email,
-    subject: 'Restablece tu cuenta en ',
-    html: `
-      <p>Hola ${nombre}, comprueba tu cuenta</p>
-
-      <p>Tu cuenta ya casi esta lista, sigue el siguiente enlace para restablecerla: 
-      <a href="${process.env.BACKEND_URL}/auth/olvide-password/${token}">Restablecer contraseña</a> </p>
-
-      <p> Si no buscas restablecer tu cuenta ignora este mensaje</p>
-    `
-  })
+  try {
+    // Enviar el email
+    const info = await transport.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Restablece tu cuenta Daniel Román Bienes Raices',
+      text: "Restablece tu cuenta Daniel Román Bienes Raices",
+      html: `
+        <p>Hola ${nombre}, comprueba tu cuenta</p>
+  
+        <p>Tu cuenta ya casi esta lista, sigue el siguiente enlace para restablecerla: 
+        <a href="${process.env.BACKEND_URL}/auth/olvide-password/${token}">Restablecer contraseña</a> </p>
+  
+        <p> Si no buscas restablecer tu cuenta ignora este mensaje</p>
+      `
+    })
+  
+    console.log("Se envío el email correctamente",info);
+  } catch (error) {
+    console.error("No se pudo enviar el email: ", error);
+  }
 }
 
 const emailNuevoMensaje = async (datos) => {
@@ -55,7 +70,7 @@ const emailNuevoMensaje = async (datos) => {
 
   // Enviar el email
   await transport.sendMail({
-    from: 'bienesraices.com',
+    from: process.env.EMAIL_USER,
     to: ownerEmail,
     subject: 'Nuevos Mensajes 🏠',
     text: 'Nuevos Mensajes 🏠',
