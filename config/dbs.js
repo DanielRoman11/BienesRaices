58f8b7d2129a65eee5fa4a59159232b98dfd3682
Daniel Román <danielroman.05111@gmail.com>
2024-02-23 11:21:47 -0500

    pool de conexiones

--- config/dbs.js
+++ config/dbs.js
@@ -9,7 +9,7 @@ const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env
     timestamps: true
   },
   pool: {
-    max: 20,
+    max: 10,
     min: 0,
     acquire: 60000,
     idle: 10000
